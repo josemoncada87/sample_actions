bce3355934ea394e13f0f072292e8af0b2a1d105
josemoncada87 <josemoncada87@hotmail.com>
2022-04-27 11:19:54 -0500

    sample

--- sample.test.js
+++ sample.test.js
@@ -3,5 +3,18 @@ function sum(a,b) {
 }
 
 test("testing sample a sum", ()=>{
-    expect(sum(2,2)).toBe(4);
+    expect(sum(2,2)).toBe(4);    
 }); 
+
+
+describe("add integers",()=>{
+    test("2 positive integers",()=>{
+        expect(sum(5,32)).toBe(37);
+    });
+    test("2 negative integers",()=>{
+        expect(sum(-5,-8)).toBe(-13);
+    });
+    test("1 positive and 1 negative integer",()=>{
+        expect(sum(10,-5)).toBe(5);
+    });    
+});
